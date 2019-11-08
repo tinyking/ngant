786bd2216b324081375907e4d1b28b650ce2f3ce
tinyking <akingchao@qq.com>
2019-11-08 13:45:51 +0800

    update ci

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('omniee-ngant app is running!');
+    expect(page.getTitleText()).toEqual('ngant-app app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'omniee-ngant'`, () => {
+  it(`should have as title 'ngant-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('omniee-ngant');
+    expect(app.title).toEqual('ngant-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('omniee-ngant app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngant-app app is running!');
   });
 });
