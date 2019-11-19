ab15779daf95b6ec5f76af0aaaf4004862ebd60f
tinyking <akingchao@qq.com>
2019-11-19 09:44:25 +0800

    test(form): 修改单元测试，创建对象增加参数 #1

--- projects/ngant/src/lib/form/form-control.directive.spec.ts
+++ projects/ngant/src/lib/form/form-control.directive.spec.ts
@@ -2,7 +2,7 @@ import { FormControlDirective } from './form-control.directive';
 
 describe('FormControlDirective', () => {
   it('should create an instance', () => {
-    const directive = new FormControlDirective(null, null);
+    const directive = new FormControlDirective(null, null, null, null, null, null, null);
     expect(directive).toBeTruthy();
   });
 });
